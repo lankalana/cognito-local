e4da285b312d16dd1db689b245a04eb15998d0bc
Leo Niemikallio <leo.niemikallio@evolver.fi>
2024-10-03 14:58:53 +0300

    Added placeholder integration tests for identityprovider actions

---
+++ integration-tests/aws-sdk/updateIdentityProvider.test.ts
@@ -0,0 +1,21 @@
+import { ClockFake } from "../../src/__tests__/clockFake";
+import { withCognitoSdk } from "./setup";
+
+const currentDate = new Date();
+const roundedDate = new Date(currentDate.getTime());
+roundedDate.setMilliseconds(0);
+
+const clock = new ClockFake(currentDate);
+
+describe(
+  "CognitoIdentityServiceProvider.updateIdentityProvider",
+  withCognitoSdk(
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    (Cognito) => {
+      it.todo("updates an identity provider");
+    },
+    {
+      clock,
+    },
+  ),
+);
